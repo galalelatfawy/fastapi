e3d83665834bbff65c90aa24153834d5508862dd
Galal Elatfawy <galal.elatfawy@gmail.com>
2021-09-19 23:02:29 +0200

    add main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional, Text
 from fastapi import FastAPI
+from pydantic import BaseModel
 app = FastAPI()
 
 
@@ -6,11 +8,33 @@ app = FastAPI()
 def index():
     return {'data': {'name':'Galal'}}
 
+@app.get ('/blog/unpublished')
+def unpublished():
+    return {'data': 'Unpublished'}
+
+@app.get ('/blog')
+def blog(limit: Optional[int]=10,published: Optional [bool]=True, sort: Optional[str] = None):
+    if published:
+
+        return {'data': f'Blog List of published {limit} topics'}
+    else:
+        return {'data': f'Blog List of {limit} topics'}
 
 @app.get ('/blog/{id}')
-def blog(id):
+def blog(id: int):
     return {'name': id}
 
+
 @app.get ('/blog/{id}/{comment}')
-def comment(comment):
+def comment(comment: str):
     return {'data': {'comment':'first comment'}}
+
+class newblog(BaseModel):
+    title: str
+    description: Optional[str] = None
+    body: str
+
+
+@app.post ('/blog')
+def newpost(request: newblog):
+    return {'data': f'New post created with title {request.title}'}
